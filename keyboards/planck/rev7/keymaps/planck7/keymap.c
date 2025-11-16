c05de27bdcfc28078b01b0705fa2673f089bf1f7
ohshitgorillas <atom.goldsmith@gmail.com>
2025-11-16 12:54:30 -0800

    removed flow tap disabling for SC_SENT (not a flow tap key anyway)

--- keyboards/planck/rev7/keymaps/planck7/keymap.c
+++ keyboards/planck/rev7/keymaps/planck7/keymap.c
@@ -132,20 +132,6 @@ void leader_end_user(void) {
 }
 
 
-// disable flow tap term for space cadet shift/enter
-uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record, uint16_t prev_keycode) {
-    if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
-        switch (keycode) {
-            case SC_SENT:
-                return 0;
-            default:
-                return FLOW_TAP_TERM;
-        }
-    }
-    return FLOW_TAP_TERM;
-}
-
-
 // space cadet shift/enter hold on other key press per key
 bool get_hold_on_other_key_press_per_key(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
