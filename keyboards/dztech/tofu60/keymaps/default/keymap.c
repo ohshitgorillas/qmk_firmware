ccde21aab1cd77086f8ac5216e05324318094266
ohshitgorillas <atom.goldsmith@gmail.com>
2025-11-16 11:15:24 -0800

    FINALLY have caps lock working as dual tap/hold configuration!

--- keyboards/dztech/tofu60/keymaps/default/keymap.c
+++ keyboards/dztech/tofu60/keymaps/default/keymap.c
@@ -137,42 +137,25 @@ void leader_end_user(void) {
 }
 
 
-// disable flow tap term for caps lock layer key
-uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record, uint16_t prev_keycode) {
-    if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
-        switch (keycode) {
-            case LT(2, KC_CAPS):
-            case LT(3, KC_CAPS):
-                return 0;
-            default:
-                return FLOW_TAP_TERM;
-        }
-    }
-    return FLOW_TAP_TERM;
-}
-
-
-// caps lock layer hold on other key press per key
 bool get_hold_on_other_key_press_per_key(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-      case LT(2,KC_CAPS):
-      case LT(3,KC_CAPS):
-          return true;
-      default:
-          return false;
-  }
+    case LT(2,KC_CAPS):
+    case LT(3,KC_CAPS):
+        return true;
+    default:
+        return false;
+    }
 }
 
 
-// default tapping term for caps lock layer key (mod taps 300ms)
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-      case LT(2,KC_CAPS):
-      case LT(3,KC_CAPS):
-          return 60;
-      default:
-          return 300;
-  }
+    switch (keycode) {
+        case LT(2,KC_CAPS):
+        case LT(3,KC_CAPS):
+            return 100;
+        default:
+            return 300;
+    }
 }
 
 
@@ -189,12 +172,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // Windows/Linux main layer
         KC_ESC,        KC_1,         KC_2,         KC_3,         KC_4,         KC_5,   KC_6,   KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
         KC_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC, KC_RBRC, KC_BSPC,
-        LT(3,KC_CAPS),  LCTL_T(KC_A), LALT_T(KC_S), LWIN_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RWIN_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,          KC_ENT,
+        LT(3,KC_CAPS), LCTL_T(KC_A), LALT_T(KC_S), LWIN_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RWIN_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,          KC_ENT,
         KC_LSFT,       KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,                  KC_RSFT, MO(4),
         KC_LCTL,       KC_LWIN,      KC_LALT,                                                  KC_SPC,                                                    KC_RALT, KC_RWIN, KC_RCTL
     ),
     [2] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         // macOS caps layer
+        // notes:
+        //     - KC_NUBS goes to Compose
+        //     - LSG(3,4,5) allows screenshotting under macOS
         XXXXXXX,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,  _______, _______,
         XXXXXXX,  KC_NUBS, KC_LCTL, KC_LSFT, KC_ENT,  XXXXXXX, KC_PGUP, KC_HOME, KC_UP,     KC_END,    KC_PSCR,   XXXXXXX,   XXXXXXX, _______,
         XXXXXXX,  KC_LOPT, KC_LCMD, KC_BSPC, KC_DEL,  XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN,   KC_RIGHT,  KC_UNDS,   KC_PLUS,            _______,
