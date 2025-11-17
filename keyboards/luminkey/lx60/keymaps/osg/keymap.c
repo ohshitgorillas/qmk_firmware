ff55babce02bfec8d8350c9e56ac15831fc02045
ohshitgorillas <atom.goldsmith@gmail.com>
2025-11-16 23:39:58 -0800

    Add new keymap for Luminkey LX60

---
+++ keyboards/luminkey/lx60/keymaps/osg/keymap.c
@@ -0,0 +1,153 @@
+/* Copyright 2022 DZTECH <moyi4681@Live.cn>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ #include QMK_KEYBOARD_H
+ #include "secrets.h"
+
+ extern uint16_t leader_sequence[];
+
+
+ void leader_end_user(void) {
+     // Personal info macros
+     if (leader_sequence_three_keys(KC_P, KC_E, KC_M)) { SEND_STRING(PERSONAL_EMAIL); }
+     else if (leader_sequence_three_keys(KC_W, KC_E, KC_M)) { SEND_STRING(WORK_EMAIL); }
+     else if (leader_sequence_three_keys(KC_P, KC_P, KC_H)) { SEND_STRING(PERSONAL_PHONE); }
+     else if (leader_sequence_three_keys(KC_W, KC_P, KC_H)) { SEND_STRING(WORK_PHONE); }
+     else if (leader_sequence_four_keys(KC_A, KC_D, KC_D, KC_R)) { SEND_STRING(PERSONAL_ADDRESS); }
+     else if (leader_sequence_three_keys(KC_O, KC_S, KC_G)) { SEND_STRING(PERSONAL_DOMAIN); }
+
+     // Go-to app shortcuts (single key)
+     else if (leader_sequence[0] != KC_NO && leader_sequence[1] == KC_NO) {
+         switch (leader_sequence[0]) {
+             case KC_Q: tap_code16(LCG(KC_F23)); break;   // WeChat
+             case KC_W: tap_code16(LALT(KC_F23)); break;  // WhatsApp
+             case KC_T: tap_code16(LSFT(KC_F23)); break;  // Teams
+             case KC_A: tap_code16(LCS(KC_F23)); break;   // Safari (Android msgs)
+             case KC_S: tap_code16(LGUI(KC_F23)); break;  // Signal
+             case KC_D: tap_code16(LCA(KC_F23)); break;   // Discord
+             case KC_F: tap_code(KC_F22); break;          // Finder
+             case KC_Z: tap_code16(LGUI(KC_F24)); break;  // Outlook
+             case KC_X: tap_code16(LSFT(KC_F24)); break;  // Terminal
+             case KC_C: tap_code16(LCTL(KC_F24)); break;  // Cursor
+             case KC_B: tap_code(KC_F14); break;          // Browser
+             case KC_N: tap_code(KC_F15); break;          // Calculator
+             case KC_M: tap_code(KC_F16); break;          // Music
+         }
+     }
+
+     // Two-key sequences
+     else if (leader_sequence_two_keys(KC_S, KC_M)) { toggle_spongemock(); }
+     else if (leader_sequence_two_keys(KC_A, KC_C)) { tap_code16(QK_AUTOCORRECT_TOGGLE); }
+     else if (leader_sequence_two_keys(KC_S, KC_C)) { tap_code16(XCASE_SNAKE); }
+     else if (leader_sequence_two_keys(KC_K, KC_C)) { tap_code16(XCASE_KEBAB); }
+     else if (leader_sequence_two_keys(KC_C, KC_C)) { tap_code16(XCASE_CAMEL); }
+
+     // xcase dynamic delimiters
+     else if (leader_sequence[0] == KC_X && leader_sequence[1] == KC_C) {
+         uint16_t delimiter = leader_sequence[2];
+         switch (delimiter) {
+             case KC_LSFT:
+             case KC_RSFT:
+
+             case OS_LSFT:
+             case OS_RSFT:
+             case SC_SENT:
+                 delimiter = LSFT(leader_sequence[3]);  // Use shifted fourth key
+                 break;
+         }
+         enable_xcase_with(delimiter);
+     }
+ }
+
+
+ bool get_hold_on_other_key_press_per_key(uint16_t keycode, keyrecord_t *record) {
+   switch (keycode) {
+     case LT(2,KC_CAPS):
+     case LT(3,KC_CAPS):
+         return true;
+     default:
+         return false;
+     }
+ }
+
+
+ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+     switch (keycode) {
+         case LT(2,KC_CAPS):
+         case LT(3,KC_CAPS):
+             return 100;
+         default:
+             return 300;
+     }
+ }
+
+
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+     [0] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+         // macOS main layer
+         KC_ESC,        KC_1,         KC_2,         KC_3,         KC_4,         KC_5,   KC_6,   KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
+         KC_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC, KC_RBRC, KC_BSPC,
+         LT(2,KC_CAPS), LCTL_T(KC_A), LALT_T(KC_S), LCMD_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RCMD_T(KC_K), ROPT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,          KC_ENT,
+         KC_LSFT,       KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,                  KC_RSFT, MO(4),
+         KC_LCTL,       KC_LOPT,      KC_LCMD,                                                  KC_SPC,                                                    KC_RCMD, KC_ROPT, KC_RCTL
+     ),
+     [1] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+         // Windows/Linux main layer
+         KC_ESC,        KC_1,         KC_2,         KC_3,         KC_4,         KC_5,   KC_6,   KC_7,         KC_8,         KC_9,         KC_0,            KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
+         KC_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC, KC_RBRC, KC_BSPC,
+         LT(3,KC_CAPS), LCTL_T(KC_A), LALT_T(KC_S), LWIN_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H,   RSFT_T(KC_J), RWIN_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,          KC_ENT,
+         KC_LSFT,       KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_N,   KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,                  KC_RSFT, MO(4),
+         KC_LCTL,       KC_LWIN,      KC_LALT,                                                  KC_SPC,                                                    KC_RALT, KC_RWIN, KC_RCTL
+     ),
+     [2] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+         // macOS caps layer
+         // notes:
+         //     - KC_NUBS goes to Compose
+         //     - LSG(3,4,5) allows screenshotting under macOS
+         XXXXXXX,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,  _______, _______,
+         XXXXXXX,  KC_NUBS, KC_LCTL, KC_LSFT, KC_ENT,  XXXXXXX, KC_PGUP, KC_HOME, KC_UP,     KC_END,    KC_PSCR,   XXXXXXX,   XXXXXXX, _______,
+         XXXXXXX,  KC_LOPT, KC_LCMD, KC_BSPC, KC_DEL,  XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN,   KC_RIGHT,  KC_UNDS,   KC_PLUS,            _______,
+         _______,  QK_LEAD, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, LSG(KC_3), LSG(KC_4), LSG(KC_5),            _______, XXXXXXX,
+         _______,  _______, _______,                                     _______,                                  _______,   _______, _______
+     ),
+      [3] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+         // Windows/Linux caps layer
+         XXXXXXX,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,  _______, _______,
+         XXXXXXX,  KC_NUBS, KC_LWIN, KC_LSFT, KC_ENT,  XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,    KC_PSCR,   XXXXXXX,   XXXXXXX, _______,
+         XXXXXXX,  KC_LCTL, KC_LALT, KC_BSPC, KC_DEL,  XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_UNDS,   KC_PLUS,            _______,
+         _______,  QK_LEAD, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,              _______, XXXXXXX,
+         _______,  _______, _______,                                     _______,                                _______,   _______, _______
+     ),
+     [4] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
+         // universal navigation layer (HHKB style w/ slight modifications)
+         XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,    KC_F12,  KC_INS,  KC_DEL,
+         QK_LEAD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_SCRL,  KC_PAUS,   KC_UP,     XXXXXXX, _______,
+         XXXXXXX,  OS_LCTL, OS_LALT, OS_LGUI, OS_LSFT, XXXXXXX, KC_ASTR, KC_SLSH, KC_HOME, KC_PGUP,  KC_LEFT,   KC_RIGHT,           _______,
+         _______,  XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_PLUS, KC_MINS, KC_END,  KC_PGDN,  KC_DOWN,              _______, XXXXXXX,
+         _______,  _______, _______,                                     _______,                               _______,   _______, _______
+     ),
+ };
+
+
+ // OS Awareness
+ bool process_detected_host_os_user(os_variant_t detected_os) {
+     if (detected_os == OS_MACOS || detected_os == OS_IOS) {
+         set_single_persistent_default_layer(0);  // macOS/iOS uses layer 0
+     } else {
+         set_single_persistent_default_layer(1);  // Everything else uses layer 1
+     }
+     return true;
+ }
