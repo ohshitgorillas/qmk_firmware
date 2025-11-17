b82d9210ad606b482c5fbe90aebdae9b950b867b
ohshitgorillas <atom.goldsmith@gmail.com>
2025-11-17 15:30:39 -0800

    Removed SC_SENT from xcase dynamic leader sequence

--- keyboards/luminkey/lx60/keymaps/osg/keymap.c
+++ keyboards/luminkey/lx60/keymaps/osg/keymap.c
@@ -27,10 +27,8 @@ void leader_end_user(void) {
         switch (delimiter) {
             case KC_LSFT:
             case KC_RSFT:
-
             case OS_LSFT:
             case OS_RSFT:
-            case SC_SENT:
                 delimiter = LSFT(leader_sequence[3]);  // Use shifted fourth key
                 break;
         }
@@ -144,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [4] = LAYOUT_60_ansi_tsangan_split_bs_rshift(
         // universal navigation layer (HHKB style w/ slight modifications)
-        XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,    KC_F12,  KC_INS,  KC_DEL,
+        XXXXXXX,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,    KC_F11,    KC_F12,  KC_INS,  KC_DEL,
         QK_LEAD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_SCRL,  KC_PAUS,   KC_UP,     XXXXXXX, _______,
         XXXXXXX,  OS_LCTL, OS_LALT, OS_LGUI, OS_LSFT, XXXXXXX, KC_ASTR, KC_SLSH, KC_HOME, KC_PGUP,  KC_LEFT,   KC_RIGHT,           _______,
         _______,  XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_PLUS, KC_MINS, KC_END,  KC_PGDN,  KC_DOWN,              _______, XXXXXXX,
